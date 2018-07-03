870d29f243459051395f453e148d171910bdbe18
Radosław Szmit <radoslawszmit@gmail.com>
2018-07-03 11:02:43 +0200

    HDP on CentOS 7

--- hdp-centos7-local-install.sh
+++ hdp-centos7-local-install.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+yum -y update
+yum -y upgrade
+
+yum install -y wget
+
+yum install -y epel-release
+
+yum -y install wget vim htop ntp openssh-server openssh-clients nano bash-completion tree
