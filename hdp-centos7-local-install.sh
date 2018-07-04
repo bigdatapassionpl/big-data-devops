b9322da5cb41ba0246d4b97a5a95fa4017013d60
Radosław Szmit <radoslawszmit@gmail.com>
2018-07-04 15:44:29 +0200

    HDP on CentOS 7

--- hdp-centos7-local-install.sh
+++ hdp-centos7-local-install.sh
@@ -8,3 +8,19 @@ yum install -y wget
 yum install -y epel-release
 
 yum -y install wget vim htop ntp openssh-server openssh-clients nano bash-completion tree
+
+hostnamectl set-hostname sandbox.hortonworks.com
+hostnamectl status
+hostname
+hostname -f
+
+echo "\n127.0.0.1   sandbox sandbox.hortonworks.com" >> /etc/hosts
+
+ssh-keygen -f id_rsa -t rsa -N ''
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
+sestatus
