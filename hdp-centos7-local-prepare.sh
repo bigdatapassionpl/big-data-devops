4000db416f7b2e431a4461efebbd1159ea19031f
Radosław Szmit <radoslawszmit@gmail.com>
2018-07-04 15:47:46 +0200

    HDP on CentOS 7

--- hdp-centos7-local-prepare.sh
+++ hdp-centos7-local-prepare.sh
@@ -9,12 +9,15 @@ yum install -y epel-release
 
 yum -y install wget vim htop ntp openssh-server openssh-clients nano bash-completion tree
 
+
 hostnamectl set-hostname sandbox.hortonworks.com
 hostnamectl status
 hostname
 hostname -f
 
+
 echo "\n127.0.0.1   sandbox sandbox.hortonworks.com" >> /etc/hosts
+
 
 ssh-keygen -f id_rsa -t rsa -N ''
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
@@ -24,3 +27,27 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0
 sestatus
+
+
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl status firewalld
+systemctl disable firewalld.service
+systemctl stop firewalld.service
+systemctl status firewalld.service
+
+
+yum install -y ntp
+systemctl disable chrony.service
+systemctl enable ntpd
+systemctl start ntpd
+systemctl status ntpd
+systemctl enable ntpd.service
+systemctl is-enabled ntpd.service
+systemctl start ntpd.service
+systemctl status ntpd.service
+
+
+umask 022
+echo umask 022 >> ~/.bash_profile
+echo umask 022 >> /etc/profile
