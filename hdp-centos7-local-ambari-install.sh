8ab3d58cf6b84149965a7ae27be0c9926f8171fb
Radosław Szmit <radoslawszmit@gmail.com>
2018-07-08 03:29:43 +0200

    HDP on CentOS 7

--- hdp-centos7-local-ambari-install.sh
+++ hdp-centos7-local-ambari-install.sh
@@ -21,3 +21,8 @@ sudo yum install mysql-connector-java* -y
 ls -al /usr/share/java/mysql-connector-java.jar
 cd /var/lib/ambari-server/resources/
 ln -s /usr/share/java/mysql-connector-java.jar mysql-connector-java.jar
+
+
+wget http://public-repo-1.hortonworks.com/HDP-UTILS-GPL-1.1.0.22/repos/centos7/extjs/extjs-2.2-1.noarch.rpm
+rpm -ivh extjs-2.2-1.noarch.rpm
+rm /usr/hdp/current/oozie-server/.prepare_war_cmd
