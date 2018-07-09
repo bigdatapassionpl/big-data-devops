a466ca58afe21f7829c93254a87705e7e9eb25ef
Radosław Szmit <radoslawszmit@gmail.com>
2018-07-09 21:30:00 +0200

    HDP on CentOS 7

--- hdp-centos7-local-ambari-install.sh
+++ hdp-centos7-local-ambari-install.sh
@@ -7,12 +7,9 @@ wget -nv http://public-repo-1.hortonworks.com/HDP/centos7/2.x/updates/2.6.5.0/hd
 yum repolist
 yum update
 
-
 yum install ambari-server -y
 
-
 ambari-server setup
-
 
 ambari-server start
 
