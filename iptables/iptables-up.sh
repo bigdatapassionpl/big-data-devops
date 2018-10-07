2c9fd5bb9c911ff9160a48c2454ae14159848503
Radosław Szmit <radoslawszmit@gmail.com>
2018-10-07 06:33:53 +0200

    Instalacja HPD na maszynach z root

--- iptables/iptables-up.sh
+++ iptables/iptables-up.sh
@@ -7,7 +7,7 @@ iptables -S
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-for e in `cat white-list.txt` ; do echo iptables -I INPUT -s ${e} -j ACCEPT; done
+for e in `cat white-list.txt` ; do iptables -I INPUT -s ${e} -j ACCEPT; done
 
 for f in `grep horton  /etc/hosts|awk '{ print $1 }'`; do iptables -I INPUT -s ${f} -j ACCEPT; done
 
