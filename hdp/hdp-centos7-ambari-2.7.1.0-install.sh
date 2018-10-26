2eccb420799023fef7b1937a337118bb8ca759fb
Radosław Szmit <radoslawszmit@gmail.com>
2018-10-26 12:28:58 +0200

    HDP 3.0

--- hdp/hdp-centos7-ambari-2.7.1.0-install.sh
+++ hdp/hdp-centos7-ambari-2.7.1.0-install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.6.2.2/ambari.repo -O /etc/yum.repos.d/ambari.repo
-
-wget -nv http://public-repo-1.hortonworks.com/HDP/centos7/2.x/updates/2.6.5.0/hdp.repo -O /etc/yum.repos.d/hdp.repo
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
 
 yum repolist
 yum update
@@ -12,14 +10,3 @@ yum install ambari-server -y
 ambari-server setup
 
 ambari-server start
-
-
-sudo yum install mysql-connector-java* -y
-ls -al /usr/share/java/mysql-connector-java.jar
-cd /var/lib/ambari-server/resources/
-ln -s /usr/share/java/mysql-connector-java.jar mysql-connector-java.jar
-
-
-wget http://public-repo-1.hortonworks.com/HDP-UTILS-GPL-1.1.0.22/repos/centos7/extjs/extjs-2.2-1.noarch.rpm
-rpm -ivh extjs-2.2-1.noarch.rpm
-rm /usr/hdp/current/oozie-server/.prepare_war_cmd
