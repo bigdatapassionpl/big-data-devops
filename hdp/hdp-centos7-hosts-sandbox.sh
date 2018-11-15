1c8a31d86733b5d605e4686f6d7d45797ac778e7
Radosław Szmit <radoslawszmit@gmail.com>
2018-11-15 19:13:25 +0100

    Instalacja HDP

--- hdp/hdp-centos7-hosts-sandbox.sh
+++ hdp/hdp-centos7-hosts-sandbox.sh
@@ -3,7 +3,15 @@
 echo "Hostname"
 hostnamectl set-hostname sandbox.hortonworks.com
 hostnamectl status
+
 hostname
 hostname -f
+
+cp /etc/hosts > /tmp/hosts
+sed '1d' /etc/hosts > /tmp/newhosts1
+sed '1d' /tmp/newhosts1 > /tmp/newhosts2
+mv /tmp/newhosts2 /etc/hosts
+
 echo -e "\n" >> /etc/hosts
 echo "127.0.0.1  sandbox.hortonworks.com" >> /etc/hosts
+echo "::1  sandbox.hortonworks.com" >> /etc/hosts
