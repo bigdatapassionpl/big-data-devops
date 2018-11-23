f7edfe1c380ad207a9285b73aef862699eea1ac4
Radosław Szmit <radoslawszmit@gmail.com>
2018-11-23 11:24:11 +0100

    Instalacja HPD na maszynach z root

--- hdp/hdp-centos7-hosts-sandbox.sh
+++ hdp/hdp-centos7-hosts-sandbox.sh
@@ -11,10 +11,10 @@ hostnamectl status
 hostname
 hostname -f
 
-cp /etc/hosts > /tmp/hosts
-sed '1d' /etc/hosts > /tmp/newhosts1
-sed '1d' /tmp/newhosts1 > /tmp/newhosts2
-mv /tmp/newhosts2 /etc/hosts
+#cp /etc/hosts > /tmp/hosts
+#sed '1d' /etc/hosts > /tmp/newhosts1
+#sed '1d' /tmp/newhosts1 > /tmp/newhosts2
+#mv /tmp/newhosts2 /etc/hosts
 
 echo -e "\n" >> /etc/hosts
 echo "127.0.0.1  sandbox.hortonworks.com" >> /etc/hosts
