2e3ae80ca5e3c1d9212e6a7d501aefd74d1252b5
Radosław Szmit <radoslawszmit@gmail.com>
2019-03-21 21:45:49 +0100

    Instalacja HDP

--- iptables/iptables-up.sh
+++ iptables/iptables-up.sh
@@ -9,7 +9,7 @@ iptables -A OUTPUT -o lo -j ACCEPT
 
 for e in `cat white-list.txt` ; do iptables -I INPUT -s ${e} -j ACCEPT; done
 
-for f in `grep horton  /etc/hosts|awk '{ print $1 }'`; do iptables -I INPUT -s ${f} -j ACCEPT; done
+for f in `grep bigdatapassion  /etc/hosts|awk '{ print $1 }'`; do iptables -I INPUT -s ${f} -j ACCEPT; done
 
 iptables -A INPUT -p tcp  --dport 22 -j  ACCEPT
 
