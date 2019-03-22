11816e0905511c247595c5ed19eea8726c539004
Radosław Szmit <radoslawszmit@gmail.com>
2019-03-22 17:55:31 +0100

    Instalacja HDP

--- hdp/hdp-centos7-hostname-set.sh
+++ hdp/hdp-centos7-hostname-set.sh
@@ -6,10 +6,23 @@ hostnamectl set-hostname "Hortonworks Worker Node" --pretty
 hostnamectl set-hostname $1 --static
 hostnamectl set-hostname $1 --transient
 
+echo
 hostnamectl status
 
+echo
 hostname
 
+echo
 hostname -f
 
+echo
 hostname -s
+
+echo
+cat /etc/hosts
+
+echo
+cat /etc/sysconfig/network
+
+echo
+cat /etc/oci-hostname.conf
