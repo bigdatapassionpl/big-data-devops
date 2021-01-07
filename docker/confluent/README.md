b7f77035487678f21ff44327b90b44aa54948f9c
Radosław Szmit <radoslawszmit@gmail.com>
2021-01-07 23:06:16 +0100

    Confluent Platform

---
+++ docker/confluent/README.md
@@ -0,0 +1,5 @@
+
+# Confluent Platform
+
+* https://docs.confluent.io/platform/current/quickstart/ce-docker-quickstart.html
+* https://github.com/confluentinc/cp-all-in-one
---
+++ docker/resolve-hosts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# get all running docker container names
+containers=$(sudo docker ps | awk '{if(NR>1) print $NF}')
+host=$(hostname)
+
+# loop through all containers
+for container in $containers
+do
+  echo "Container: $container"
+
+  container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container)
+  echo $container_ip
+
+  container_hostname=$(docker inspect -f '{{.Config.Hostname}}' $container)
+  echo $container_hostname
+
+  echo "$container_ip $container_hostname" >> /etc/hosts
+
+  echo ================================
+done
+
+
+
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' zookeeper
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka1
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka2
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka3
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka-manager
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' schema-registry
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rest-proxy
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka-topics-ui
+#
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' schema-registry-ui
