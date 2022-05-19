827c8da05fcf632eb19b099eb5e16dae8afa98e4
Radosław Szmit <radoslawszmit@gmail.com>
2022-05-19 10:05:51 +0200

    AWS EC2 script

--- cloud/aws/update-ec2-instances-in-hosts.sh
+++ cloud/aws/update-ec2-instances-in-hosts.sh
@@ -3,9 +3,12 @@
 profile=$1
 echo "profile: $profile"
 
+sudo cp /etc/hosts /etc/hosts-bkp-"`date +"%Y-%m-%d-%H-%M-%S"`"
+
 aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicDnsName,PublicIpAddress,PrivateDnsName,PrivateIpAddress]" --output table  --profile $profile
 
 sudo sed '/compute.internal/d' /etc/hosts > /tmp/newhosts
 sudo cp /tmp/newhosts /etc/hosts
+
 aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress,PrivateDnsName]" --output text  --profile $profile > /tmp/aws-hosts
 sudo sh -c 'cat /tmp/aws-hosts | grep -v None >> /etc/hosts'
