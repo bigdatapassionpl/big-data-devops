163da0d3a2c3e242b94e35069163616d5c025b96
Radosław Szmit <radoslawszmit@gmail.com>
2022-05-20 12:14:45 +0200

    EMR

--- cloud/aws/emr/emr-create-cluster.sh
+++ cloud/aws/emr/emr-create-cluster.sh
@@ -5,13 +5,13 @@ aws emr create-cluster \
   --auto-scaling-role EMR_AutoScaling_DefaultRole \
   --applications Name=Spark \
   --ebs-root-volume-size 10 \
-  --ec2-attributes '{"KeyName":"vockey","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-1451ea4b","EmrManagedSlaveSecurityGroup":"sg-08f208da5bc56f56c","EmrManagedMasterSecurityGroup":"sg-0151288ddcff4e6a2"}' \
+  --ec2-attributes file://emr-ec2-attributes.json \
   --service-role EMR_DefaultRole \
   --enable-debugging \
   --release-label emr-5.31.0 \
   --log-uri 's3n://aws-logs-109824097611-us-east-1/elasticmapreduce/' \
-  --steps file://emr-steps.json \
   --instance-groups file://emr-instance-groups.json \
-  --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
   --region us-east-1 \
-  --profile default
+  --profile default \
+  --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
+  --steps file://emr-steps.json
