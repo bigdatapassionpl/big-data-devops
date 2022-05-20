24b84ade5fff438399e9cfe2a2fee1c1c1e03587
Radosław Szmit <radoslawszmit@gmail.com>
2022-05-20 12:19:15 +0200

    EMR

--- cloud/aws/emr/emr-create-cluster.sh
+++ cloud/aws/emr/emr-create-cluster.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 aws emr create-cluster \
-  --name 'EMR Spark Cluster' \
+  --name 'EMR Cluster' \
+  --os-release-label 2.0.20220406.1 \
   --auto-scaling-role EMR_AutoScaling_DefaultRole \
-  --applications Name=Spark \
-  --ebs-root-volume-size 10 \
+  --applications Name=Hive Name=Hue Name=Hadoop Name=JupyterHub Name=JupyterEnterpriseGateway Name=Zeppelin Name=Oozie Name=Tez Name=Spark Name=Livy Name=Ganglia Name=Presto \
+  --ebs-root-volume-size 15 \
   --ec2-attributes file://emr-ec2-attributes.json \
   --service-role EMR_DefaultRole \
   --enable-debugging \
-  --release-label emr-5.31.0 \
-  --log-uri 's3n://aws-logs-109824097611-us-east-1/elasticmapreduce/' \
+  --release-label emr-6.6.0 \
+  --log-uri 's3n://aws-logs-434239389032-us-east-1/elasticmapreduce/' \
   --instance-groups file://emr-instance-groups.json \
   --region us-east-1 \
   --profile default \
+  --auto-termination-policy '{"IdleTimeout":14400}' \
   --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
   --steps file://emr-steps.json
