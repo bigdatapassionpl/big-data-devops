bcbdefbe7fede3f041f66f407454783f9c5fcb65
Radosław Szmit <radoslawszmit@gmail.com>
2023-01-28 22:16:17 +0100

    Spark on K8s

---
+++ kubernetes/helm/yunikorn/yunikorn.md
@@ -0,0 +1,6 @@
+
+~~~shell
+
+kubectl port-forward svc/yunikorn-service 9889:9889 -n yunikorn
+
+~~~
---
+++ kubernetes/helm/yunikorn/yunikorn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export HELM_REPO_NAME=yunikorn
+export HELM_REPO_URL=https://apache.github.io/yunikorn-release
+export HELM_CHART=yunikorn
+export HELM_CHART_VERSION=1.1.0
