aac05f943e367511622b01fc2abcc6dd395d8825
Radosław Szmit <radoslawszmit@gmail.com>
2023-01-28 22:16:45 +0100

    Spark on K8s

---
+++ kubernetes/helm/helm-search.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo Script name: $0
+echo $# arguments
+if [ $# -ne 1 ];
+    then echo "illegal number of parameters"
+    exit
+fi
+
+source $1/$1.sh
+
+export HELM_DIR=$HELM_CHART
+export HELM_NAMESPACE=$HELM_CHART
+export HELM_RELEASE=$HELM_REPO_NAME
+
+helm repo add $HELM_REPO_NAME $HELM_REPO_URL
+helm repo list
+helm repo update
+
+echo "VERSION: "$HELM_CHART_VERSION
+helm search repo $HELM_REPO_NAME
+
+helm search repo $HELM_REPO_NAME --versions | head
+
+helm search repo $HELM_REPO_NAME --versions --devel | head
