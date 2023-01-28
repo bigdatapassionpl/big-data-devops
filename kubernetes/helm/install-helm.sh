c6ad0ab52ed641bdec0e5283dd0df04a84aafce5
Radosław Szmit <radoslawszmit@gmail.com>
2023-01-28 22:05:13 +0100

    Spark on K8s

--- kubernetes/helm/install-helm.sh
+++ kubernetes/helm/install-helm.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo Script name: $0
+echo $# arguments
+if [ $# -ne 1 ];
+    then echo "illegal number of parameters"
+    exit
+fi
+
+source $1/$1.sh
+
 export HELM_DIR=$HELM_CHART
 export HELM_NAMESPACE=$HELM_CHART
 export HELM_RELEASE=$HELM_REPO_NAME
