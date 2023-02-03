1ae5cd16be98601d56ebb7906092c258ec6e19ed
Radosław Szmit <radoslawszmit@gmail.com>
2023-02-03 13:07:11 +0100

    Ambari

--- docker/images/ambari/ambari-setup.sh
+++ docker/images/ambari/ambari-setup.sh
@@ -6,17 +6,25 @@ wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bi
 tar -zxf apache-maven-3.8.7-bin.tar.gz
 mv apache-maven-3.8.7 maven
 
-export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+#export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export M2_HOME=/opt/maven
 export PATH=${M2_HOME}/bin:${PATH}
 mvn --version
 
+alias python=python2.7
+ln -s /usr/bin/python2.7 /usr/bin/python
+
 cd /opt/
 
-wget https://www-eu.apache.org/dist/ambari/ambari-2.7.7/apache-ambari-2.7.7-src.tar.gz
-tar -zxf apache-ambari-2.7.7-src.tar.gz
+#wget https://www-eu.apache.org/dist/ambari/ambari-2.7.7/apache-ambari-2.7.7-src.tar.gz
+#tar -zxf apache-ambari-2.7.7-src.tar.gz
+#cd /opt/apache-ambari-2.7.7-src
 
-cd /opt/apache-ambari-2.7.7-src
+git clone https://github.com/apache/ambari.git
+cd /opt/ambari
+#git checkout branch-2.7.7
+
 mvn versions:set -DnewVersion=2.7.7.0.0
 
 pushd ambari-metrics
@@ -26,3 +34,4 @@ popd
 mvn -B clean install jdeb:jdeb -DnewVersion=2.7.7.0.0 -DbuildNumber=388e072381e71c7755673b7743531c03a4d61be8 -DskipTests -Dpython.ver="python >= 2.6"
 
 #apt-get install ./ambari-server*.deb
+apt-get install ./ambari-server/target/ambari-server_3.0.0.0-SNAPSHOT-dist.deb
--- docker/images/ambari/ambari.md
+++ docker/images/ambari/ambari.md
@@ -9,5 +9,8 @@ docker rmi -f $DOCKER_IMAGE
 docker build --rm -t $DOCKER_IMAGE .
 docker images | grep bigdatapassion
 docker rm $DOCKER_NAME
-docker run -it --name=$DOCKER_NAME -p 8080:8080 $DOCKER_IMAGE bash
+docker run -it --name=$DOCKER_NAME \
+  -p 8080:8080 \
+  -v $(pwd)/m2:/root/.m2 \
+  $DOCKER_IMAGE bash
 ~~~
