3ec2bba8b03cf982266895d9d40e9313505a862b
Radosław Szmit <radoslawszmit@gmail.com>
2023-03-19 16:17:04 +0100

    Helm upgrade

--- kubernetes/helm/helm-search.sh
+++ kubernetes/helm/helm-search.sh
@@ -2,16 +2,16 @@
 
 echo Script name: $0
 echo $# arguments
-if [ $# -ne 1 ];
+if [ $# -ne 2 ];
     then echo "illegal number of parameters"
     exit
 fi
 
-source $1/$1.sh
+source $1/$2/$2.sh
 
-export HELM_DIR=$HELM_CHART
+export HELM_DIR=$HELM_REPO_NAME/$HELM_CHART
 export HELM_NAMESPACE=$HELM_CHART
-export HELM_RELEASE=$HELM_REPO_NAME
+export HELM_RELEASE=$HELM_REPO_NAME-$HELM_CHART
 
 helm repo remove $HELM_REPO_NAME
 helm repo add $HELM_REPO_NAME $HELM_REPO_URL
@@ -21,6 +21,6 @@ helm repo update
 echo "VERSION: "$HELM_CHART_VERSION
 helm search repo $HELM_REPO_NAME
 
-helm search repo $HELM_REPO_NAME --versions | head
+helm search repo $HELM_REPO_NAME/$HELM_CHART --versions | head
 
-helm search repo $HELM_REPO_NAME --versions --devel | head
+helm search repo $HELM_REPO_NAME/$HELM_CHART --versions --devel | head
