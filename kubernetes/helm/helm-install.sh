4b5b2c145970d61604c30e316915cdc36651a506
Radosław Szmit <radoslawszmit@gmail.com>
2023-06-20 13:54:13 +0200

    Minikube

--- kubernetes/helm/helm-install.sh
+++ kubernetes/helm/helm-install.sh
@@ -2,32 +2,43 @@
 
 echo Script name: $0
 echo $# arguments
-if [ $# -ne 1 ];
-    then echo "illegal number of parameters"
-    exit
+if [ $# -eq 1 ]
+then
+  echo $1
+  source $1/$1.sh
+  export HELM_DIR=$1
+elif [ $# -eq 2 ]
+then
+  echo "$1 $2"
+  source $1-$2/$2.sh
+  export HELM_DIR=$1-$2
+else
+  echo "illegal number of parameters"
+  exit
 fi
 
-source $1/$1.sh
+if [ -z "$HELM_NAMESPACE" ]; then export HELM_NAMESPACE=$HELM_CHART; fi
+if [ -z "$HELM_RELEASE" ]; then export HELM_RELEASE=$HELM_REPO_NAME; fi
 
-export HELM_DIR=$HELM_CHART
-export HELM_NAMESPACE=$HELM_CHART
-export HELM_RELEASE=$HELM_REPO_NAME
+echo "HELM_REPO_NAME=$HELM_REPO_NAME"
+echo "HELM_REPO_URL=$HELM_REPO_URL"
+echo "HELM_CHART=$HELM_CHART"
+echo "HELM_CHART_VERSION=$HELM_CHART_VERSION"
+echo "HELM_DIR=$HELM_DIR"
+echo "HELM_NAMESPACE=$HELM_NAMESPACE"
+echo "HELM_RELEASE=$HELM_RELEASE"
+
+
 
 helm repo remove $HELM_REPO_NAME
 helm repo add $HELM_REPO_NAME $HELM_REPO_URL
 helm repo list
 helm repo update
 
-echo "VERSION: "$HELM_CHART_VERSION
-helm search repo $HELM_REPO_NAME/$HELM_CHART
-
-helm show values $HELM_REPO_NAME/$HELM_CHART --version=$HELM_CHART_VERSION > $HELM_DIR/values-default.yaml
-
 time helm upgrade $HELM_RELEASE $HELM_REPO_NAME/$HELM_CHART \
   --install \
   --timeout 10m \
   --cleanup-on-fail \
-  --create-namespace \
   --namespace $HELM_NAMESPACE \
   --values $HELM_DIR/values.yaml \
   --version=$HELM_CHART_VERSION
--- kubernetes/helm/helm-uninstall.sh
+++ kubernetes/helm/helm-uninstall.sh
@@ -2,16 +2,33 @@
 
 echo Script name: $0
 echo $# arguments
-if [ $# -ne 1 ];
-    then echo "illegal number of parameters"
-    exit
+if [ $# -eq 1 ]
+then
+  echo $1
+  source $1/$1.sh
+  export HELM_DIR=$1
+elif [ $# -eq 2 ]
+then
+  echo "$1 $2"
+  source $1-$2/$2.sh
+  export HELM_DIR=$1-$2
+else
+  echo "illegal number of parameters"
+  exit
 fi
 
-source $1/$1.sh
+if [ -z "$HELM_NAMESPACE" ]; then export HELM_NAMESPACE=$HELM_CHART; fi
+if [ -z "$HELM_RELEASE" ]; then export HELM_RELEASE=$HELM_REPO_NAME; fi
 
-export HELM_DIR=$HELM_CHART
-export HELM_NAMESPACE=$HELM_CHART
-export HELM_RELEASE=$HELM_REPO_NAME
+echo "HELM_REPO_NAME=$HELM_REPO_NAME"
+echo "HELM_REPO_URL=$HELM_REPO_URL"
+echo "HELM_CHART=$HELM_CHART"
+echo "HELM_CHART_VERSION=$HELM_CHART_VERSION"
+echo "HELM_DIR=$HELM_DIR"
+echo "HELM_NAMESPACE=$HELM_NAMESPACE"
+echo "HELM_RELEASE=$HELM_RELEASE"
+
+
 
 helm uninstall $HELM_RELEASE \
   --namespace $HELM_NAMESPACE
