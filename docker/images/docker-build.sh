b4f3ef4a00ebaddf3fe1433bd8ea919a0be756b2
Radosław Szmit <radoslawszmit@gmail.com>
2023-06-24 15:26:42 +0200

    Docker build

--- docker/images/docker-build.sh
+++ docker/images/docker-build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo Script name: $0
+echo $# arguments
+if [ $# -eq 1 ]
+then
+  echo $1
+  export DOCKER_NAME=$1
+else
+  echo "illegal number of parameters"
+  exit
+fi
+
 export DOCKER_USER=bigdatapassion
-export DOCKER_NAME=coder
 export DOCKER_VERSION=latest
 
 export DOCKER_IMAGE="${DOCKER_USER}/${DOCKER_NAME}:${DOCKER_VERSION}"
@@ -9,7 +19,7 @@ echo $DOCKER_IMAGE
 
 docker rmi -f $DOCKER_IMAGE
 
-docker build --rm -t $DOCKER_IMAGE coder/
+docker build --rm -t $DOCKER_IMAGE $DOCKER_NAME/
 
 docker images | grep $DOCKER_NAME
 
