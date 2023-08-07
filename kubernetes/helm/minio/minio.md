aeac09609ee84cef19cba0c09acadc817f53e5a0
Radosław Szmit <radoslawszmit@gmail.com>
2023-08-07 12:11:07 +0200

    MinIO Helm Chart

---
+++ kubernetes/helm/minio/minio.md
@@ -0,0 +1,4 @@
+
+* https://github.com/minio/charts
+* https://github.com/minio/minio/tree/master/helm/minio
+* https://github.com/minio/operator/tree/master
---
+++ kubernetes/helm/minio/minio.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export HELM_REPO_NAME=minio
+export HELM_REPO_URL=https://charts.min.io
+export HELM_CHART=minio
+export HELM_CHART_VERSION=5.0.13
