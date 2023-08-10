20cdf501015adf382ca2ad0e7b1e31c51e61c17c
Radosław Szmit <radoslawszmit@gmail.com>
2023-08-10 19:28:45 +0200

    Superset Docker image

--- docker/images/docker-run.sh
+++ docker/images/docker-run.sh
@@ -17,10 +17,9 @@ export DOCKER_VERSION=latest
 export DOCKER_IMAGE="${DOCKER_USER}/${DOCKER_NAME}:${DOCKER_VERSION}"
 echo $DOCKER_IMAGE
 
-docker rmi -f $DOCKER_IMAGE
-
-docker build --rm -t $DOCKER_IMAGE $DOCKER_NAME/
-
 docker images | grep $DOCKER_NAME
 
-docker push $DOCKER_IMAGE
+docker run -it --name=$DOCKER_NAME \
+  $DOCKER_IMAGE bash
+
+docker rm $DOCKER_NAME
