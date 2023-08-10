9bed0c2b5398f31603973c7c7581d994df625a49
Radosław Szmit <radoslawszmit@gmail.com>
2023-08-10 19:42:00 +0200

    Superset Docker image

--- docker/images/docker-build.sh
+++ docker/images/docker-build.sh
@@ -6,13 +6,18 @@ if [ $# -eq 1 ]
 then
   echo $1
   export DOCKER_NAME=$1
+  export DOCKER_VERSION=latest
+elif [ $# -eq 2 ]
+then
+  echo "$1 $2"
+  export DOCKER_NAME=$1
+  export DOCKER_VERSION=$2
 else
   echo "illegal number of parameters"
   exit
 fi
 
 export DOCKER_USER=bigdatapassion
-export DOCKER_VERSION=latest
 
 export DOCKER_IMAGE="${DOCKER_USER}/${DOCKER_NAME}:${DOCKER_VERSION}"
 echo $DOCKER_IMAGE
