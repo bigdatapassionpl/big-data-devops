e5cf0e7e1154dae367d58a1796aeeb711ef2f1d4
Radosław Szmit <radoslawszmit@gmail.com>
2023-09-27 19:02:26 +0200

    Apache Yunikorn

--- kubernetes/config-yaml/spark/spark-submit-k8s.sh
+++ kubernetes/config-yaml/spark/spark-submit-k8s.sh
@@ -5,13 +5,15 @@ echo $K8S_CLUSTER_NAME
 export K8S_CLUSTER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$K8S_CLUSTER_NAME\")].cluster.server}")
 echo $K8S_CLUSTER
 
-kubectl get pods -n k8s-jobs --no-headers=true | awk '/spark-pi/{print $1}' | xargs  kubectl delete -n k8s-jobs pod
+#kubectl get pods -n k8s-jobs --no-headers=true | awk '/spark-pi/{print $1}' | xargs  kubectl delete -n k8s-jobs pod
 
 export SPARK_HOME="/Users/radek/programs/spark/spark-3.5.0-bin-hadoop3"
 export SPARK_IMAGE="gcr.io/spark-operator/spark:v3.1.1-hadoop3"
 export SPARK_APP="local:///opt/spark/examples/jars/spark-examples_2.12-3.1.1.jar"
-export SPARK_ARGS="10000"
-export SPARK_EXECUTORS=10
+export SPARK_ARGS="100000"
+export SPARK_EXECUTORS=7
+
+export K8S_NS=k8s-jobs-dev
 
 $SPARK_HOME/bin/spark-submit \
     --master k8s://$K8S_CLUSTER \
@@ -23,6 +25,6 @@ $SPARK_HOME/bin/spark-submit \
     --conf spark.executor.memory=1g \
     --conf spark.kubernetes.executor.request.cores=1 \
     --conf spark.kubernetes.container.image=$SPARK_IMAGE \
-    --conf spark.kubernetes.namespace=k8s-jobs \
+    --conf spark.kubernetes.namespace=$K8S_NS \
     --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
     $SPARK_APP $SPARK_ARGS
--- kubernetes/config-yaml/spark/spark-submit-yunikorn.sh
+++ kubernetes/config-yaml/spark/spark-submit-yunikorn.sh
@@ -5,13 +5,15 @@ echo $K8S_CLUSTER_NAME
 export K8S_CLUSTER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$K8S_CLUSTER_NAME\")].cluster.server}")
 echo $K8S_CLUSTER
 
-kubectl get pods -n k8s-jobs --no-headers=true | awk '/spark-pi/{print $1}' | xargs  kubectl delete -n k8s-jobs pod
+#kubectl get pods -n k8s-jobs --no-headers=true | awk '/spark-pi/{print $1}' | xargs  kubectl delete -n k8s-jobs pod
 
 export SPARK_HOME="/Users/radek/programs/spark/spark-3.5.0-bin-hadoop3"
 export SPARK_IMAGE="gcr.io/spark-operator/spark:v3.1.1-hadoop3"
 export SPARK_APP="local:///opt/spark/examples/jars/spark-examples_2.12-3.1.1.jar"
-export SPARK_ARGS="10000"
-export SPARK_EXECUTORS=10
+export SPARK_ARGS="100000"
+export SPARK_EXECUTORS=7
+
+export K8S_NS=k8s-jobs
 
 $SPARK_HOME/bin/spark-submit \
     --master k8s://$K8S_CLUSTER \
@@ -23,11 +25,11 @@ $SPARK_HOME/bin/spark-submit \
     --conf spark.executor.memory=1g \
     --conf spark.kubernetes.executor.request.cores=1 \
     --conf spark.kubernetes.container.image=$SPARK_IMAGE \
-    --conf spark.kubernetes.namespace=k8s-jobs \
+    --conf spark.kubernetes.namespace=$K8S_NS \
     --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
     --conf spark.kubernetes.scheduler.name=yunikorn \
-    --conf spark.kubernetes.driver.label.queue=root.k8s-jobs \
-    --conf spark.kubernetes.executor.label.queue=root.k8s-jobs \
+    --conf spark.kubernetes.driver.label.queue=root.$K8S_NS \
+    --conf spark.kubernetes.executor.label.queue=root.$K8S_NS \
     --conf spark.kubernetes.driver.annotation.yunikorn.apache.org/app-id={{APP_ID}} \
     --conf spark.kubernetes.executor.annotation.yunikorn.apache.org/app-id={{APP_ID}} \
     $SPARK_APP $SPARK_ARGS
