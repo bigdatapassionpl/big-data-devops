7d373bde38b1c67ad3693af80da75448baf63ab1
Radosław Szmit <radoslawszmit@gmail.com>
2023-10-19 13:02:26 +0200

    Spark k8s

--- kubernetes/config-yaml/spark/spark-submit-k8s.sh
+++ kubernetes/config-yaml/spark/spark-submit-k8s.sh
@@ -5,6 +5,8 @@ echo $K8S_CLUSTER_NAME
 export K8S_CLUSTER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$K8S_CLUSTER_NAME\")].cluster.server}")
 echo $K8S_CLUSTER
 
+export K8S_CLUSTER="https://2BB517F48032BC9BC992634C587CD6D1.gr7.us-east-1.eks.amazonaws.com"
+
 export SPARK_HOME="/Users/radek/programs/spark/spark-3.5.0-bin-hadoop3"
 export SPARK_IMAGE="gcr.io/spark-operator/spark:v3.1.1-hadoop3"
 export SPARK_APP="local:///opt/spark/examples/jars/spark-examples_2.12-3.1.1.jar"
@@ -14,6 +16,7 @@ export SPARK_EXECUTORS=$2
 export SPARK_ARGS=$3
 
 export CORES=500m
+export CORES=1
 export MEMORY=1g
 
 $SPARK_HOME/bin/spark-submit \
@@ -24,7 +27,7 @@ $SPARK_HOME/bin/spark-submit \
     --conf spark.kubernetes.submission.waitAppCompletion=false \
     --conf spark.executor.instances=$SPARK_EXECUTORS \
     --conf spark.driver.memory=$MEMORY \
-    --conf spark.executor.memory$MEMORY \
+    --conf spark.executor.memory=$MEMORY \
     --conf spark.kubernetes.driver.request.cores=$CORES \
     --conf spark.kubernetes.driver.request.limit.cores=$CORES \
     --conf spark.kubernetes.executor.request.cores=$CORES \
