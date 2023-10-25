5d01ee46abe47ef8df0e5d4509824db2db147642
Radosław Szmit <radoslawszmit@gmail.com>
2023-10-25 15:05:44 +0200

    Spark k8s

--- docker/images/spark/install-spark.sh
+++ docker/images/spark/install-spark.sh
@@ -4,10 +4,9 @@ export SPARK_HOME=/opt/spark
 
 # OS preparation
 set -ex && \
-sed -i 's/http:\/\/deb.\(.*\)/https:\/\/deb.\1/g' /etc/apt/sources.list && \
 apt-get update && \
 ln -s /lib /lib64 && \
-apt install -y wget bash tini libc6 libpam-modules krb5-user libnss3 procps && \
+apt install -y bash tini libc6 libpam-modules krb5-user libnss3 procps net-tools && \
 mkdir -p /opt/spark && \
 mkdir -p /opt/spark/examples && \
 mkdir -p /opt/spark/work-dir && \
@@ -16,7 +15,7 @@ rm /bin/sh && \
 ln -sv /bin/bash /bin/sh && \
 echo "auth required pam_wheel.so use_uid" >> /etc/pam.d/su && \
 chgrp root /etc/passwd && chmod ug+rw /etc/passwd && \
-rm -rf /var/cache/apt/*
+rm -rf /var/cache/apt/* && rm -rf /var/lib/apt/lists/*
 
 # OS preparation - PySpark
 mkdir ${SPARK_HOME}/python
@@ -24,10 +23,10 @@ apt-get update && \
 apt install -y python3 python3-pip && \
 pip3 install --upgrade pip setuptools && \
 # Removed the .cache to save space
-rm -r /root/.cache && rm -rf /var/cache/apt/*
+rm -rf /root/.cache && rm -rf /var/cache/apt/* && rm -rf /var/lib/apt/lists/*
 
 # Install Apache Spark
-export SPARK_VERSION="3.3.2"
+export SPARK_VERSION="3.5.0"
 export SPARK_BUNDLE=spark-${SPARK_VERSION}-bin-hadoop3
 #export SPARK_BUNDLE=spark-${SPARK_VERSION}-bin-hadoop3-scala2.13.tgz
 
@@ -35,14 +34,14 @@ wget --quiet https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/${SPAR
 tar xzf /opt/spark/${SPARK_BUNDLE}.tgz -C /opt/spark
 
 export SPARK_SOURCE="/opt/spark/${SPARK_BUNDLE}"
-mv ${SPARK_SOURCE}/jars /opt/spark/
-mv ${SPARK_SOURCE}/bin /opt/spark/
-mv ${SPARK_SOURCE}/sbin /opt/spark/
+mv ${SPARK_SOURCE}/jars /opt/spark/jars
+mv ${SPARK_SOURCE}/bin /opt/spark/bin
+mv ${SPARK_SOURCE}/sbin /opt/spark/sbin
 mv ${SPARK_SOURCE}/kubernetes/dockerfiles/spark/entrypoint.sh /opt/
 mv ${SPARK_SOURCE}/kubernetes/dockerfiles/spark/decom.sh /opt/
-mv ${SPARK_SOURCE}/examples /opt/spark/
-mv ${SPARK_SOURCE}/kubernetes/tests /opt/spark/
-mv ${SPARK_SOURCE}/data /opt/spark/
+mv ${SPARK_SOURCE}/examples /opt/spark/examples
+mv ${SPARK_SOURCE}/kubernetes/tests /opt/spark/tests
+mv ${SPARK_SOURCE}/data /opt/spark/data
 
 mv ${SPARK_SOURCE}/python/pyspark ${SPARK_HOME}/python/pyspark
 mv ${SPARK_SOURCE}/python/lib ${SPARK_HOME}/python/lib
