622f33364c7c956305f50e5818f75fab1752db44
Radosław Szmit <radoslawszmit@gmail.com>
2023-11-21 15:02:05 +0100

    Spark k8s

--- kubernetes/helm/yunikorn/yunikorn.sh
+++ kubernetes/helm/yunikorn/yunikorn.sh
@@ -3,4 +3,4 @@
 export HELM_REPO_NAME=yunikorn
 export HELM_REPO_URL=https://apache.github.io/yunikorn-release
 export HELM_CHART=yunikorn
-export HELM_CHART_VERSION=1.3.0
+export HELM_CHART_VERSION=1.4.0
