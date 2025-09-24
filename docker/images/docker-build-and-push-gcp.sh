8de1df63c2e2a8d441524ef0ad32bcf7191ceb80
Radosław Szmit <radoslaw.szmit@gmail.com>
2025-09-24 13:56:26 +0200

    Kafka Connect update

---
+++ docker/images/docker-build-and-push-gcp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo Script name: $0
+echo $# arguments
+if [ $# -eq 1 ]
+then
+  echo $1
+  export DOCKER_NAME=$1
+  export DOCKER_VERSION=latest
+elif [ $# -eq 2 ]
+then
+  echo "$1 $2"
+  export DOCKER_NAME=$1
+  export DOCKER_VERSION=$2
+else
+  echo "illegal number of parameters"
+  exit
+fi
+
+gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://$GCP_REGION-docker.pkg.dev
+
+export FULL_IMAGE_NAME="$GCP_REGION-docker.pkg.dev/$GCP_PROJECT_ID/$REPO_NAME/$IMAGE_NAME"
+echo $FULL_IMAGE_NAME
+
+docker rmi -f $FULL_IMAGE_NAME
+
+docker build --rm -t $FULL_IMAGE_NAME --platform linux/amd64 $DOCKER_NAME/
+
+docker push $FULL_IMAGE_NAME
+
+docker images | grep $FULL_IMAGE_NAME
